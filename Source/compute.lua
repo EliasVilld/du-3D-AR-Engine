ea2d09b281ed609672e2a2fb6f464a2a2f9e6167
EliasVilld <82938532+EliasVilld@users.noreply.github.com>
2022-02-09 23:25:37 +0100

    v0.14 | Updated compute.lua
     - Added metrics monitoring for debug purpose
     - Added some optimization to largely reduce operations count
     - Added past vertices computation reuse.
     - Added better depth sorting

--- Source/compute.lua
+++ Source/compute.lua
@@ -1,5 +1,13 @@
 -- To place in a tick event with a timerId "compute"
 --==================================================
+local logCompTime = _logs['Computation Time']
+local logCompMem = _logs['Computation Memory Use']
+local logCompNormals = _logs['Computed Normals']
+local logCompFaces = _logs['Computed Faces']
+local logCompVertices = _logs['Computed Vertices']
+
+local t0 = system.getTime()
+
 -- Localisation
 local cos, sin, tan = math.cos, math.sin, math.tan
 local deg, rad = math.deg, math.rad
@@ -9,15 +17,17 @@ local width = system.getScreenWidth()
 local height = system.getScreenHeight()
 local vFov = system.getCameraVerticalFov()
 
-local near = 0.1
-local far = 1000.0
+if width ~= _width or height~= _height or vFov~= _vFov then
+    _width, _height, _vFov = width, height, vFov
+    
+    _aspectRatio = height/width
+    _tanFov = 1.0/math.tan(math.rad(vFov)*0.5)
+    _field = -_far/(_far-_near)
+end
 
-local aspectRatio = height/width
-local tanFov = 1.0/math.tan(math.rad(vFov)*0.5)
-local field = -far/(far-near)
-
-local af = aspectRatio*tanFov
-local nq = near*field
+local tanFov, field = _tanFov, _field
+local af = _aspectRatio*tanFov
+local nq = _near*field
 
 
 -- Get camera data
@@ -30,14 +40,20 @@ local camRgtX, camRgtY, camRgtZ = rgt[1], rgt[2], rgt[3]
 local up = system.getCameraUp()
 local camUpX, camUpY, camUpZ = up[1], up[2], up[3]
 
--- Set model properties
+-- Set mesh properties
 local yaw, pitch, roll = 0, 0, 0
-local oX, oY, oZ = 0-0.125, 12-0.125, -0.125
+local oX, oY, oZ = -0.125, 11.875, -0.125
 local scaleX, scaleY, scaleZ = 1, 1, 1
 
+-- Set model
+local nMX, nMY, nMZ = mesh.normals[1], mesh.normals[2], mesh.normals[3]
+local vMX, vMY, vMZ = mesh.vertices[1], mesh.vertices[2], mesh.vertices[3]
+local MF, ML = mesh.faces, mesh.lines
+
+local sV, sN, sF, sL = #vMX,#nMX,#MF,#ML
 _model = {
-    size = {sV, sN, sV},
-    vert = {
+    size = {sV, sN, sF, sL},
+    vertices = {
         {},
         {},
         {}
@@ -48,24 +64,28 @@ _model = {
         {},
         {}
     },
+    lines = {},
     faces = {}
 }
+local vX, vY, vZ = _model.vertices[1], _model.vertices[2], _model.vertices[3]
+local nX, nY, nZ, nDot = _model.normals[1], _model.normals[2], _model.normals[3], _model.normals[4]
+local F, L = _model.faces, _model.lines
 
 -- Precompute cos and sin
-local cy, sy = math.cos(yaw), math.sin(yaw)
-local cp, sp = math.cos(pitch), math.sin(pitch)
-local cr, sr = math.cos(roll), math.sin(roll)
+local cy, sy = cos(yaw), sin(yaw)
+local cp, sp = cos(pitch), sin(pitch)
+local cr, sr = cos(roll), sin(roll)
 
 local cycp, sycp = cy*cp, sy*cp
 local cyspsr_sycr, syspsr_cycr, cpsr = cy*sp*sr - sy*cr, sy*sp*sr + cy*cr, cp*sr
 local cyspcr_sysr, syspcr_cysr, cpcr = cy*sp*cr + sy*sr, sy*sp*cr - cy*sr, cp*cr
 
+local dX, dY, dZ = oX - camX, oY - camY, oZ - camZ
 
 -- Compute normals
-local nX, nY, nZ = mesh.normals[1], mesh.normals[2], mesh.normals[3]
-for i=1,#nX do
-    local nx, ny, nz = nX[i], nY[i], nZ[i]
-    
+for i=1,sN do
+    local nx, ny, nz = nMX[i], nMY[i], nMZ[i]
+
     --Rotation
     local nxT, nyT, nzT = nx, ny, nz
     nx = nxT*cycp + nyT*sycp + nzT*(-sp)
@@ -73,78 +93,106 @@ for i=1,#nX do
     nz = nxT*cyspcr_sysr + nyT*syspcr_cysr + nzT*cpcr
 
     --Add the normal update the normal vector
-    _model.normals[1][i] = nx
-    _model.normals[2][i] = ny
-    _model.normals[3][i] = nz
-    _model.normals[4][i] = nil
+    nX[i] = nx
+    nY[i] = ny
+    nZ[i] = nz
+    nDot[i] = nil
 end
+logCompNormals[#logCompNormals+1] = sN
 
 -- Compute vertices
 local faceId = 1
-local F = mesh.faces
-local vX, vY, vZ = mesh.vert[1], mesh.vert[2], mesh.vert[3]
-
-for i = 1,#F do
-    local face, depth, dot = F[i], 0, 0
+local vertexIndex = 0
+for i = 1,sF do
+    local face, depth, dot = MF[i], 0, 0
     local nId = face[2]
-    
+
     for j=1,#face[1] do
         local id = face[1][j]
 
-        --Compute this vertice
-        vx, vy, vz = vX[id], vY[id], vZ[id]
+        if j>1 and vX[id] then
+            vz = vZ[id]
+        else
+            --Compute this vertice
+            vx, vy, vz = vMX[id], vMY[id], vMZ[id]
 
-        --Local scaling
-        vx = vx*scaleX
-        vy = vy*scaleY
-        vz = vz*scaleZ
+            --Local scaling
+            vx = vx*scaleX
+            vy = vy*scaleY
+            vz = vz*scaleZ
 
-        --Local rotations YAW, PITCH, ROLL
-        local vxT, vyT, vzT = vx, vy, vz
-        vx = vxT*cycp + vyT*sycp + vzT*(-sp)
-        vy = vxT*cyspsr_sycr + vyT*syspsr_cycr + vzT*cpsr
-        vz = vxT*cyspcr_sysr + vyT*syspcr_cysr + vzT*cpcr
+            --Local rotations YAW, PITCH, ROLL
+            local vxT, vyT, vzT = vx, vy, vz
+            vx = vxT*cycp + vyT*sycp + vzT*(-sp)
+            vy = vxT*cyspsr_sycr + vyT*syspsr_cycr + vzT*cpsr
+            vz = vxT*cyspcr_sysr + vyT*syspcr_cysr + vzT*cpcr
 
-        --Positionning
-        vx = vx + oX - camX
-        vy = vy + oY - camY
-        vz = vz + oZ - camZ
+            --Positionning
+            vx = vx + dX
+            vy = vy + dY
+            vz = vz + dZ
 
-        -- Compute dot product with normal
-        dot = _model.normals[4][nId]
-        if not dot then
-            local len = (vx*vx+vy*vy+vz*vz)^(-0.5)
-            dot = vx*len*nX[nId] + vy*len*nY[nId] + vz*len*nZ[nId]
+            -- Compute dot product with normal
+            dot = nDot[nId]
+            if not dot then
+                local len = (vx*vx+vy*vy+vz*vz)^(-0.5)
+                dot = vx*len*nMX[nId] + vy*len*nMY[nId] + vz*len*nMZ[nId]
+                nDot[nId] = dot
+            end
+
+            if dot >= 0 then goto skipFace end
+
+            --Project camera referential
+            vxT, vyT, vzT = vx, vy, vz
+            vx = vxT * camRgtX + vyT * camRgtY + vzT * camRgtZ
+            vy = vxT * camFwdX + vyT * camFwdY + vzT * camFwdZ
+            vz = vxT *  camUpX + vyT *  camUpY + vzT *  camUpZ
+
+            if vy < 0.01 then goto skipFace end
+            
+            --Project vertice from 3D -> 2D
+            vxT, vyT, vzT = vx, vy, vz
+            local ivyT = 1/vyT
+            vx = (af * vxT)*ivyT
+            vy = ( -tanFov * vzT)*ivyT
+            vz = ( -field * vyT + nq)*ivyT
+
+            --Add this vertice to the mesh
+            vX[id], vY[id], vZ[id] = vx, vy, vz
+            
+            vertexIndex = vertexIndex +1
         end
-        _model.normals[4][nId] = dot
-        
-        if dot >= 0 then break end
-
-        --Project camera referential
-        vxT, vyT, vzT = vx, vy, vz
-        vx = vxT * camRgtX + vyT * camRgtY + vzT * camRgtZ
-        vy = vxT * camFwdX + vyT * camFwdY + vzT * camFwdZ
-        vz = vxT *  camUpX + vyT *  camUpY + vzT *  camUpZ
-
-        --Project vertice from 3D -> 2D
-        vxT, vyT, vzT = vx, vy, vz
-        vx = (af * vxT)/vyT
-        vy = ( -tanFov * vzT)/vyT
-        vz = ( -field * vyT + nq)/vyT
-
-        --Add this vertice to the mesh
-        _model.vert[1][id], _model.vert[2][id], _model.vert[3][id] = vx, vy, vz
 
         depth = depth + vz
     end
-
-
-    face[3] = depth/#face[1]
-    if face[3] >= 0 and dot < 0 then
-        face[4] = mesh.colors[nId] or {180, 180, 180}
-        _model.faces[faceId] = face
-        faceId = faceId +1
+    
+    depth = depth/#face[1]
+    if depth >= 0 then
+        F[faceId] = {
+            face[1],
+            nId,
+            depth,
+            mesh.colors[nId] or {180, 180, 180}
+        }
+        faceId = faceId+1
     end
+    
+    ::skipFace::
 end
 
-table.sort(_model.faces, function(a,b) return a[3] > b[3] end)
+logCompFaces[#logCompFaces+1] = faceId-1
+logCompVertices[#logCompVertices+1] = vertexIndex
+
+table.sort(F, function(a,b) return a[3] > b[3] end)
+
+
+logCompTime[#logCompTime+1] = system.getTime() - t0
+logCompMem[#logCompMem+1] = collectgarbage("count")
+
+if #logCompTime > 200 then table.remove(logCompTime, 1) end
+if #logCompMem > 200 then table.remove(logCompMem, 1) end
+if #logCompNormals > 200 then table.remove(logCompNormals, 1) end
+if #logCompFaces > 200 then table.remove(logCompFaces, 1) end
+if #logCompVertices > 200 then table.remove(logCompVertices, 1) end
+
+--collectgarbage("collect")
